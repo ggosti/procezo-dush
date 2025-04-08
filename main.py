fa219fc27f9e0600fc4702f993abdd0bb671a676
Giorgio G <giogosti@gmail.com>
2025-04-08 14:50:24 +0200

    set up initial openAPI endpoints

--- main.py
+++ main.py
@@ -49,21 +49,41 @@ app.mount("/edit_group", WSGIMiddleware(edit_group.server))
 
 # Define the main API endpoint
 @app.get("/")
-def index():
+async def index():
     return "Hello"
 
+# define the API endpoint to get all projects
+@app.get('/projects') 
+async def get_projects() -> dict[str, list[str]]: #dict[str, list[str]]:
+    """Get all projects"""
+    rawProjects = projObj.projects
+    rawProjectsNames = [p.name for p in rawProjects if p.step == 'raw'] 
+    #print("rawProjects",[{'name':p.name,'step':p.step} for p in rawProjects])
+    return {'projects':rawProjectsNames}
+
 # Define the API endpoint to get all groups in project
-@app.get('/projects/{project_name}')
-def get_project(project_name):
+@app.get('/groups/{project_name}')
+async def get_project(project_name:str) -> dict[str, list[str]] | dict[str, str]:
     """Get all groups in a project"""
-    print("project_name",project_name)#,allowedProjects)
+    #print("project_name",project_name)#,allowedProjects)
     if project_name in allowedProjects:
         rawProjectGroups = projObj.get_groups_in_project(project_name,'raw')
-        return {'groups':rawProjectGroups}
+        return {'groups':[g.name for g in rawProjectGroups]}
+    else:
+        return {'error':'Project not allowed'}
+    
+# Define the API endpoint to get all records in group
+@app.get('/records/{project_name}/{group_name}')
+async def get_group(project_name:str, group_name:str) -> dict[str, list[str]] | dict[str, str]:
+    """Get all records in a group"""
+    #print("group_name",group_name)
+    if project_name in allowedProjects:
+        rawGroupRecords = projObj.get_recods_in_project_and_group(project_name,group_name,'raw',version=None) # get_records_in_group(project_name,group_name,'raw')
+        return {'records':[r.name for r in rawGroupRecords]}
     else:
         return {'error':'Project not allowed'}
 
 
 # Start the FastAPI server
 if __name__ == "__main__":
-    uvicorn.run(app, port=8087, host="0.0.0.0")
+    uvicorn.run(app, port=8087, host="0.0.0.0",)
